e5eae81fb0f639476963c24af988e2ceb77c48a3
Levent Ali <lebreeze@gmail.com>
2015-01-29 12:14:07 +0000

    ui complete

---
+++ README.md
@@ -0,0 +1,3 @@
+# Storm - Treehouse Track
+
+[REST - get JSON and parse](https://github.com/levent/Stormy/tree/rest-get-with-json-to-dict)
--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -12,6 +12,13 @@ class ViewController: UIViewController {
     
     private let apiKey = "b9a141348c7d140edae994e2e92f7c29" // TODO: Move this out of source control and reset API KEY
 
+    @IBOutlet weak var iconView: UIImageView!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var rainLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,7 +32,17 @@ class ViewController: UIViewController {
             if error == nil {
                 let dataObject = NSData(contentsOfURL: location)
                 let weatherDictionary = NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as NSDictionary
-//                println(weatherDictionary)
+
+                let currentWeather = Current(weatherDictionary: weatherDictionary)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.tempLabel.text = "\(currentWeather.temperature)"
+                    self.iconView.image = currentWeather.icon!
+                    self.currentTimeLabel.text = "At \(currentWeather.currentTime!) it is"
+                    self.rainLabel.text = "\(currentWeather.precipProbability)"
+                    self.humidityLabel.text = "\(currentWeather.humidity)"
+                    self.summaryLabel.text = currentWeather.summary
+                })
             }
             
         }
